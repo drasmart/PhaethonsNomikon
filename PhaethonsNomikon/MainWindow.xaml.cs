5ea9ac42caf82c263ad502c56ae3867f9c5dbf19
Maksym Kucherov <maximus.the0@gmail.com>
2025-05-24 20:39:38 +0300

    Resizable footer.

--- PhaethonsNomikon/MainWindow.xaml.cs
+++ PhaethonsNomikon/MainWindow.xaml.cs
@@ -16,8 +16,43 @@ namespace PhaethonsNomikon;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private readonly double _logBoxMinHeight;
+    
     public MainWindow()
     {
         InitializeComponent();
+        _logBoxMinHeight = LogContainer.RowDefinitions[2].MinHeight;
+    }
+    
+    private void ToggleLogView_Checked(object sender, RoutedEventArgs e)
+    {
+        if (LogViewBox != null)
+        {
+            LogViewBox.Visibility = Visibility.Visible;
+        }
+        if (LogContainer?.RowDefinitions != null && LogContainer.RowDefinitions.Count >= 3)
+        {
+            LogContainer.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Star); // Main content fills space
+            LogContainer.RowDefinitions[1].Height = new GridLength(5); // Show splitter
+            // Show log view
+            LogContainer.RowDefinitions[2].MinHeight = _logBoxMinHeight;
+            LogContainer.RowDefinitions[2].Height = new GridLength(1, GridUnitType.Star);
+        }
+    }
+
+    private void ToggleLogView_Unchecked(object sender, RoutedEventArgs e)
+    {
+        if (LogViewBox != null)
+        {
+            LogViewBox.Visibility = Visibility.Collapsed;
+        }
+        if (LogContainer?.RowDefinitions != null && LogContainer.RowDefinitions.Count >= 3)
+        {
+            LogContainer.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Star); // Main content fills space
+            LogContainer.RowDefinitions[1].Height = new GridLength(0); // Hide splitter
+            // Collapse log view
+            LogContainer.RowDefinitions[2].MinHeight = 0;
+            LogContainer.RowDefinitions[2].Height = new GridLength(0);
+        }
     }
 }
