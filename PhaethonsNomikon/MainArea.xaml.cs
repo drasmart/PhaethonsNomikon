7dfc9e6bc5fd470efd7435dd8def1840ee108437
Maksym Kucherov <maximus.the0@gmail.com>
2025-05-24 20:53:17 +0300

    Introduce `MainArea` control.

---
+++ PhaethonsNomikon/MainArea.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace PhaethonsNomikon;
+
+public partial class MainArea : UserControl
+{
+    public MainArea()
+    {
+        InitializeComponent();
+    }
+}
